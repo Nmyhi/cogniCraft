f7cac53e65a058157fb0561c1d7769aef5052e32
Mihaly Lovrencsics <lomysa@gmail.com>
2023-08-26 12:21:50 +0000

    Add semantic html tags

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,32 +3,30 @@
 var userName;
 var timeScoreValue;
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "play") {
-                playGame();
-                } else {
-                if (this.getAttribute("data-type") === "hiScores") {
-                    hiScores();   
-                } else {
-                    if (this.getAttribute("data-type") === "help") {
-                        help();
-                        }
-                    }
-                }
-            });
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "play") {
+        playGame();
+      } else {
+        if (this.getAttribute("data-type") === "hiScores") {
+          hiScores();
+        } else {
+          if (this.getAttribute("data-type") === "help") {
+            help();
+          }
         }
+      }
     });
+  }
+});
 
 /**
  * This function is controlling the game
  */
 function playGame() {
-    let gameContent = `
+  let gameContent = `
     <div id = "fullscreen-container">
     <div id = "header">
     <h1 id="title"><img src="assets/images/logo.png" alt="game logo" width="50" height="50">cogniCraft</h1>
@@ -68,168 +66,171 @@ function playGame() {
     </div>
     </div>
     <script src="assets/javascript/"></script>`;
-    document.body.innerHTML = gameContent;
-   
-    //This reloads the index.html, basically it takes back to the menu.
-    
-    let menu = document.getElementsByClassName("menu");
-    menu[0].addEventListener("click", function(){
-    window.location.href = 'index.html';
-    });
-    //variables
-    //timer
-    let startTime = null;
-    let endTime = null;
-    let timerInterval;
-    //clickable
-    let clickable = false;
+  document.body.innerHTML = gameContent;
 
-    //Username input window takes the value and stores it in the userName variable
-    let userNameWindow = document.getElementById("usernamewindow");
-    let userNameInput = document.getElementById("usernameinput");
-    userNameInput.addEventListener("keydown", function(event)  {
-        if (event.key === "Enter") {
-            //Whitespace val.
-            let enteredUserName = userNameInput.value.trim();
-            if (enteredUserName === "") {
-                alert("Please enter a valid username!");
-            } else {
-                userName = enteredUserName;
-            
+  //This reloads the index.html, basically it takes back to the menu.
 
-            userNameWindow.style.display = "none";
+  let menu = document.getElementsByClassName("menu");
+  menu[0].addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+  //variables
+  //timer
+  let startTime = null;
+  let endTime = null;
+  let timerInterval;
+  //clickable
+  let clickable = false;
+
+  //Username input window takes the value and stores it in the userName variable
+  let userNameWindow = document.getElementById("usernamewindow");
+  let userNameInput = document.getElementById("usernameinput");
+  userNameInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      //Whitespace val.
+      let enteredUserName = userNameInput.value.trim();
+      if (enteredUserName === "") {
+        alert("Please enter a valid username!");
+      } else {
+        userName = enteredUserName;
+
+        userNameWindow.style.display = "none";
         let userNameDisplay = document.getElementById("usernamedisplay");
         userNameDisplay.innerHTML = `USERNAME:<br>${userName}`;
         clickable = true;
         //start the timer
         startTime = Date.now();
         timerInterval = setInterval(updateTimer, 1000);
-        }
+      }
     }
-    });
-    //Username display paragraph displays the username
+  });
+  //Username display paragraph displays the username
 
+  let colors = [
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "white",
+    "black",
+    "pink",
+    "purple",
+  ];
+  let colorsPickList = [...colors, ...colors];
+  let tiles = document.getElementsByClassName("tile");
 
-    let colors = ["red", "yellow", "green", "blue", "white", "black", "pink", "purple"];
-    let colorsPickList = [...colors, ...colors];
-    let tiles = document.getElementsByClassName("tile");
+  // Create a shuffled copy of the colorsPickList
+  let shuffledPickList = [...colorsPickList].sort(() => Math.random() - 0.5);
+  let firstTile = null; //variable to store the first clicked tile
 
-    // Create a shuffled copy of the colorsPickList
-    let shuffledPickList = [...colorsPickList].sort(() => Math.random() - 0.5);
-    let firstTile = null; //variable to store the first clicked tile
+  for (let i = 0; i < tiles.length; i++) {
+    tiles[i].setAttribute("data-color", `${shuffledPickList[i]}`);
+    tiles[i].setAttribute("data-revealed", "false"); //adding data-type attributes to the tiles in a random manner
+    tiles[i].addEventListener("click", function () {
+      if (!clickable || this.getAttribute("data-revealed") === "true") {
+        return; //already revealed, ignore
+      }
+      let color = this.getAttribute("data-color");
+      this.style.backgroundColor = color;
+      this.setAttribute("data-revealed", "true");
 
-    for (let i = 0; i < tiles.length; i++ ) {
-        tiles[i].setAttribute("data-color", `${shuffledPickList[i]}`);
-        tiles[i].setAttribute("data-revealed", "false");       //adding data-type attributes to the tiles in a random manner
-        tiles[i].addEventListener("click", function() {
-            if (!clickable || this.getAttribute("data-revealed") === "true") {
-                return;     //already revealed, ignore
-            }
-            let color = this.getAttribute("data-color");
-            this.style.backgroundColor = color;
+      if (firstTile === null) {
+        //first tile clicked store it
+        firstTile = this;
+      } else {
+        clickable = false;
+        //second tile clicked, compare to the first
+        if (
+          this.getAttribute("data-color") ===
+          firstTile.getAttribute("data-color")
+        ) {
+          //match
+          setTimeout(() => {
             this.setAttribute("data-revealed", "true");
+            firstTile.setAttribute("data-revealed", "true");
+            clickable = true; // Re-enable clicking
+            firstTile = null;
+            if (
+              document.querySelectorAll('[data-revealed="true"]').length ===
+              tiles.length
+            ) {
+              // All pairs are revealed, stop the timer
+              endTime = Date.now();
+              let endTimeValue = document.getElementById("timer").textContent;
+              let userNameValue =
+                document.getElementById("usernamedisplay").textContent;
 
-            if (firstTile === null) {
-                //first tile clicked store it
-                firstTile = this;
-            } else {
-                clickable = false;
-                //second tile clicked, compare to the first
-                if (this.getAttribute("data-color") === firstTile.getAttribute("data-color")) {
-                    //match 
-                    setTimeout(() => {
-                        this.setAttribute("data-revealed", "true");
-                        firstTile.setAttribute("data-revealed", "true");
-                        clickable = true; // Re-enable clicking
-                        firstTile = null;
-                        if (document.querySelectorAll('[data-revealed="true"]').length === tiles.length) {
-                            // All pairs are revealed, stop the timer
-                            endTime = Date.now();
-                            let endTimeValue = document.getElementById("timer").textContent;
-                            let userNameValue = document.getElementById("usernamedisplay").textContent;
-
-                            userNameWindow.innerHTML = `<button id="save">Save&Reset</button><span>Congratulations!</span><br><br><span>${userNameValue}</span><br><br><span>Time:${endTimeValue}</span>`;
-                            userNameWindow.style.display = "block";
-                            let saveButton = document.getElementById("save");
-                            saveButton.addEventListener("click", function() {
-                                
-                                timeScoreValue = document.getElementById("timer").textContent;
-                                userNameWindow.style.display = "none";
-                                saveHighScore(userName, parseInt(endTimeValue));
-                                playGame();
-                                
-
-
-                            });
-
-                        }
-                    }, 1000); // Delay for 1 second to show the matched colors
-    
-                    // Reset the firstTile variable
-                    
-                } else {
-                    setTimeout(() => {
-                        this.style.backgroundColor = document.body.style.backgroundColor;
-                        this.setAttribute("data-revealed", "false");
-                        firstTile.style.backgroundColor = document.body.style.backgroundColor;
-                        firstTile.setAttribute("data-revealed", "false");
-                        firstTile = null;
-                        clickable = true; // Re-enable clicking
-                    }, 1000); 
-                }
+              userNameWindow.innerHTML = `<button id="save">Save&Reset</button><span>Congratulations!</span><br><br><span>${userNameValue}</span><br><br><span>Time:${endTimeValue}</span>`;
+              userNameWindow.style.display = "block";
+              let saveButton = document.getElementById("save");
+              saveButton.addEventListener("click", function () {
+                timeScoreValue = document.getElementById("timer").textContent;
+                userNameWindow.style.display = "none";
+                saveHighScore(userName, parseInt(endTimeValue));
+                playGame();
+              });
             }
-        });   
+          }, 1000); // Delay for 1 second to show the matched colors
+
+          // Reset the firstTile variable
+        } else {
+          setTimeout(() => {
+            this.style.backgroundColor = document.body.style.backgroundColor;
+            this.setAttribute("data-revealed", "false");
+            firstTile.style.backgroundColor =
+              document.body.style.backgroundColor;
+            firstTile.setAttribute("data-revealed", "false");
+            firstTile = null;
+            clickable = true; // Re-enable clicking
+          }, 1000);
+        }
+      }
+    });
+  }
+
+  /**
+   * this function updates the timer
+   *
+   */
+
+  function updateTimer() {
+    if (endTime) {
+      //game ended stop updating the timer
+      clearInterval(timerInterval);
+      return;
     }
 
-/**
- * this function updates the timer
- * 
- */
-    
-    function updateTimer() {
-        if (endTime) {
-            //game ended stop updating the timer
-            clearInterval(timerInterval);
-            return;
-        }
-    
-        const currentTime = Date.now();
-        const elapsedTime = Math.floor((currentTime - startTime) / 1000);
-        const timerDisplay = document.getElementById("timer");
-        timerDisplay.textContent = elapsedTime + " ";
-    };
+    const currentTime = Date.now();
+    const elapsedTime = Math.floor((currentTime - startTime) / 1000);
+    const timerDisplay = document.getElementById("timer");
+    timerDisplay.textContent = elapsedTime + " ";
+  }
 
-    /**
- * This function stores the username and highscore in the localstorage
- */
+  /**
+   * This function stores the username and highscore in the localstorage
+   */
 
-function saveHighScore (userName, timeScoreValue) {
+  function saveHighScore(userName, timeScoreValue) {
     //retrieve existing high score from the localstore
     let highScores = JSON.parse(localStorage.getItem(`highScores`)) || [];
     //add the current game's highscore
-    highScores.push({userName, timeScoreValue});
+    highScores.push({ userName, timeScoreValue });
     //sort the highscores by timescorevalue in descending order
-    highScores.sort((a , b) => a.timeScoreValue - b.timeScoreValue);
+    highScores.sort((a, b) => a.timeScoreValue - b.timeScoreValue);
     //keep only 10 highest scores
     highScores = highScores.slice(0, 10);
     //save the updated highscores back to the localstore
     localStorage.setItem(`highScores`, JSON.stringify(highScores));
     console.log(timeScoreValue);
+  }
 }
-};
-
-
-
-
-
-
 
 /**
  * This function controls the Hi-scores menu.
  * It stores the hi scores DOM and keeps the user times
  */
 function hiScores() {
-    let hiScoresContent = `
+  let hiScoresContent = `
     <div id = "fullscreen-container">
     <div id = "header">
     <h1 id="title"><img src="assets/images/logo.png" alt="game logo" width="50" height="50">cogniCraft</h1>
@@ -262,42 +263,41 @@ function hiScores() {
 
 <!--Link to the Javascript file-->
 <script src="assets/javascript/index.js"></script> `;
-    document.body.innerHTML = hiScoresContent;
-    //This reloads the index.html, basically it takes back to the menu.
-    let menu = document.getElementsByClassName("menu");
-    menu[0].addEventListener("click", function(){
-    window.location.href = 'index.html';
-});
+  document.body.innerHTML = hiScoresContent;
+  //This reloads the index.html, basically it takes back to the menu.
+  let menu = document.getElementsByClassName("menu");
+  menu[0].addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
 
-displayHighScores();
+  displayHighScores();
 
+  /**
+   * This function displays the highscores using data stored in the localstorage
+   */
 
-/**
- * This function displays the highscores using data stored in the localstorage
- */
-
-function displayHighScores() {
+  function displayHighScores() {
     let highScores = JSON.parse(localStorage.getItem(`highScores`)) || [];
-    let highScoresTable = '<h2>High Scores</h2><table><tr><th>Username</th><th>Time (seconds)</th></tr>';
+    let highScoresTable =
+      "<h2>High Scores</h2><table><tr><th>Username</th><th>Time (seconds)</th></tr>";
     for (let score of highScores) {
-        highScoresTable += `<tr><td>${score.userName}</td><td>${score.timeScoreValue}</td></tr>`;
+      highScoresTable += `<tr><td>${score.userName}</td><td>${score.timeScoreValue}</td></tr>`;
     }
-    highScoresTable += '</table>';
+    highScoresTable += "</table>";
     // Display the highscore table on your menu page
     let highScoreTableHtml = document.querySelector(".highscores");
     highScoreTableHtml.innerHTML = highScoresTable;
     console.log(highScoresTable);
-    console.log(highScoreTableHtml);   
-};
-};
+    console.log(highScoreTableHtml);
+  }
+}
 
 /**
  * This function loads the help menu DOM.
  */
 
-
 function help() {
-    let helpContent = `
+  let helpContent = `
     <div id = "fullscreen-container">
     <div id = "header">
     <h1 id="title"><img src="assets/images/logo.png" alt="game logo" width="50" height="50">cogniCraft</h1>
@@ -332,11 +332,10 @@ function help() {
 
 <!--Link to the Javascript file-->
 <script src="assets/javascript/"></script>`;
-document.body.innerHTML = helpContent;
-    //This reloads the index.html, basically it takes back to the menu.
-    let menu = document.getElementsByClassName("menu");
-    menu[0].addEventListener("click", function(){
-    window.location.href = 'index.html';
-});
+  document.body.innerHTML = helpContent;
+  //This reloads the index.html, basically it takes back to the menu.
+  let menu = document.getElementsByClassName("menu");
+  menu[0].addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
 }
-
